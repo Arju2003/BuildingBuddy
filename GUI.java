9e8da26293eab2706782997f8c85b33f2eba9583
Daniel Michael Gomes <dgomes9@uwo.ca>
2023-02-24 15:38:53 -0500

    Initial Commit

--- GUI.java
+++ GUI.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
 public class GUI {
+    public static void main(String[] args) {
+        JFrame frame =new JFrame();  // creating instance of JFrame
+
+        JButton button =new JButton("the button.");  // creating instance of JButton
+        button.setBounds(600,350,100, 40);  // x axis, y axis, width, height
+
+        frame.add(button);//adding button in JFrame
+
+        frame.setSize(1200,700);  // 400 width and 500 height
+        frame.setLayout(null);  // using no layout managers
+        frame.setVisible(true);  // making the frame visible
+    }
 }
