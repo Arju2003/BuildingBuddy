6d355c2f389f993f6b56341cbf41206043ecd27d
Daniel Michael Gomes <dgomes9@uwo.ca>
2023-03-09 18:51:34 -0500

    Changes by arjun

--- UserData.java
+++ UserData.java
@@ -1,2 +1,19 @@
+import java.util.LinkedList;
+
 public class UserData extends Data {
+
+    private LinkedList<Data> booksMarks = new LinkedList<Data>();
+
+    public UserData() {
+
+    }
+    public void addBookmark(int poiData) {
+
+    }
+
+    public void deleteBookmark(int poiData) {
+
+    }
+
+
 }
