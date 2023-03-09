67b12ff5d9dd3772e44e0fe855d298a17e29bfbe
Jason Barrett Shew <jshew3@uwo.ca>
2023-03-09 17:48:28 -0500

    GUI, Container

--- GUI.java
+++ GUI.java
@@ -17,22 +17,22 @@ public class GUI {
             try {
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setJMenuBar(appMenu.load());
-                Container primary = new Container("./images/mc_hero.jpg");
+                Container splashScreen = new Container("./images/mc_hero.jpg");
                 ImageIcon icon = new ImageIcon("./images/bb_icon.png");
                 Image image = icon.getImage(); // transform it
                 Image newimg = image.getScaledInstance(200, 200,  Image.SCALE_SMOOTH); // scale it the smooth way
                 icon = new ImageIcon(newimg);  // transform it back
                 JLabel logo = new JLabel();
                 logo.setIcon(icon);
-                primary.load(logo);
+                splashScreen.load(logo);
                 String[] buildings = {"Middlesex College", "Kresge Building", "Physics & Astronomy"};
 
                 JComboBox<? extends String> buildingSelector = new JComboBox<>(buildings);
                 buildingSelector.setBounds(450, 300, 200, 30);
-                primary.load(buildingSelector);
+                splashScreen.load(buildingSelector);
 
                 JButton exploreButton = new JButton("Explore");
-                primary.load(exploreButton);
+                splashScreen.load(exploreButton);
 
                 frame.pack();
                 frame.setLocationRelativeTo(null); // always loads the interface at the center of the monitor regardless resolution
