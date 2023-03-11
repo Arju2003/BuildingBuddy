c23823dadf7690d1acf07cdd6c338d89e086f4fd
Jason Barrett Shew <jshew3@uwo.ca>
2023-03-11 06:27:07 -0500

    Done PopupView

--- src/ca/uwo/csteam14/PopupView.java
+++ src/ca/uwo/csteam14/PopupView.java
@@ -1,13 +1,65 @@
 package ca.uwo.csteam14;
 
-public class PopupView {
-    private String message;
+import javax.swing.*;
+import java.awt.event.*;
 
-    public void loadMessage(String message) {
+public class PopupView extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JLabel title;
+    private JFormattedTextField content;
 
+    public PopupView() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+        setLocationRelativeTo(null); // always loads the interface at the center of the monitor regardless resolution
+
+        title.setText("HELP / ABOUT");
+        content.setText("HELP INFO / ABOUT INFO GOES HERE.");
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    public void unloadMessage(int id) {
+    private void onOK() {
+        // add your code here
+        dispose();
+    }
 
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        PopupView dialog = new PopupView();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
     }
 }
