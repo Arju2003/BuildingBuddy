897fff6c64758fb9fdfdd12ed2d204550ac16799
Jason Barrett Shew <jshew3@uwo.ca>
2023-03-14 03:30:58 -0400

    Floor selector improvement. Now you don't need a button to navigate through floors. Also, the size of the main window is kept constant.

--- src/ca/uwo/csteam14/PopupView.java
+++ src/ca/uwo/csteam14/PopupView.java
@@ -30,56 +30,55 @@ public class PopupView extends JDialog {
 
         if (Objects.equals(type, "Help")) {
             title.setText("HELP");
-            content.setText("What is a POI?\n" +
-                    "\n" +
-                    "A POI is a point of interest, namely a location on the map.\n" +
-                    "\n" +
-                    "What is a bookmark?\n" +
-                    "\n" +
-                    "A bookmark is one of your favourite POIs\n" +
-                    "\n" +
-                    "What is My Location?\n" +
-                    "\n" +
-                    "My Location is a POI you defined, not a built-in POI.\n" +
-                    "\n" +
-                    "How to view or edit bookmarks?\n" +
-                    "\n" +
-                    "Enter View – Bookmarks, or press CTRL + B.\n" +
-                    "\n" +
-                    "How to view or edit My Locations?\t\n" +
-                    "\n" +
-                    "Enter View – My Locations, or press CTRL + L. \n" +
-                    "\n" +
-                    "How to choose another building?\n" +
-                    "\n" +
-                    "Click Start in the menu on top.\n"+
-                    "How to quit the application safely?\n" +
-                    "\n" +
-                    "Choose Exit from the main menu, or just click [X] on top of the window.\n" +
-                    "\n" +
-                    "I still need help!\n" +
-                    "Feel free to write us: jason@shew.cc");
+            content.setText("""
+                    What is a POI?
+
+                    A POI is a point of interest, namely a location on the map.
+
+                    What is a bookmark?
+
+                    A bookmark is one of your favourite POIs
+
+                    What is My Location?
+
+                    My Location is a POI you defined, not a built-in POI.
+
+                    How to view or edit bookmarks?
+
+                    Enter View – Bookmarks, or press CTRL + B.
+
+                    How to view or edit My Locations?
+
+                    Enter View – My Locations, or press CTRL + L.
+
+                    How to choose another building?
+
+                    Click Start in the menu on top.
+                    How to quit the application safely?
+
+                    Choose Exit from the main menu, or just click [X] on top of the window.
+
+                    I still need help!
+                    Feel free to write us: jason@shew.cc""");
         }
         else if (Objects.equals(type, "About")) {
             title.setText("ABOUT");
-            content.setText("BuildingBuddy\n" +
-                    "\n" +
-                    "Version: 1.0\n" +
-                    "\n" +
-                    "Developed by Team 14 at UWO\n" +
-                    "\n" +
-                    "Developers: Daniel, Robert, Joshua, Arjuna, Jason\n" +
-                    "\n" +
-                    "Project Website: https://wiki.csd.uwo.ca/display/COMPSCI2212W2023GROUP14/COMPSCI+2212+-+Winter+2023+-+Group+14+Home)\nGitHub: https://github.com/dan1el5/BuildingBuddy");
+            content.setText("""
+                    BuildingBuddy
+
+                    Version: 1.0
+
+                    Developed by Team 14 at UWO
+
+                    Developers: Daniel, Robert, Joshua, Arjuna, Jason
+
+                    Project Website: https://wiki.csd.uwo.ca/display/COMPSCI2212W2023GROUP14/COMPSCI+2212+-+Winter+2023+-+Group+14+Home)
+                    GitHub: https://github.com/dan1el5/BuildingBuddy""");
         }
 
 
         button.setText("Close");
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onOK();
-            }
-        });
+        button.addActionListener(e -> onOK());
 
 
         // call onCancel() when cross is clicked
@@ -91,11 +90,7 @@ public class PopupView extends JDialog {
         });
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                onCancel();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
     private void onOK() {
