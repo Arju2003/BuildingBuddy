842ab963f549c70c38df4b0c9f0c53c120fe2521
Daniel Michael Gomes <dgomes9@uwo.ca>
2023-03-20 15:21:51 -0400

    Changes by arjun

--- src/ca/uwo/csteam14/Data.java
+++ src/ca/uwo/csteam14/Data.java
@@ -1,5 +1,12 @@
 package ca.uwo.csteam14;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 
 public class Data {
@@ -8,6 +15,47 @@ public class Data {
     private boolean canRead;
     private boolean canWrite;
     private LinkedList createdPOIs;
+    private String POIId;
+    private String building;
+    private String buildingCode;
+    private String floor;
+    private int roomNumber;
+    private String category;
+    private String description;
+    private int mapx;
+    private int mapy;
+    private boolean builtIn;
+
+    public Data () {
+
+            JSONParser parser = new JSONParser();
+
+            try {
+
+                Object obj = parser.parse(new FileReader("user.json"));
+                JSONObject jObject = (JSONObject) obj;
+
+                POIId = (String) jObject.get("POIId");
+                building = (String) jObject.get("building");
+                buildingCode = (String) jObject.get("buildingCode");
+                floor = (String) jObject.get("floor");
+                roomNumber = (Integer) jObject.get("roomNumber");
+                category = (String) jObject.get("category");
+                description = (String) jObject.get("description");
+                mapx = (Integer) jObject.get("mapX");
+                mapy = (Integer) jObject.get("mapY");
+                builtIn = (Boolean)  jObject.get("builtIn");
+
+
+
+            }
+            catch (FileNotFoundException e) { e.printStackTrace(); }
+            catch (IOException e) { e.printStackTrace(); }
+            catch (ParseException e) { e.printStackTrace(); }
+            catch (Exception e) { e.printStackTrace(); }
+    }
+
+
 
     public void lockFile() {
         canWrite = false;
--- src/ca/uwo/csteam14/UserData.java
+++ src/ca/uwo/csteam14/UserData.java
@@ -12,46 +12,7 @@ import org.json.simple.parser.ParseException;
 public class UserData extends Data {
 
     private LinkedList<Data> booksMarks = new LinkedList<Data>();
-    private String POIId;
-    private String building;
-    private String buildingCode;
-    private String floor;
-    private int roomNumber;
-    private String category;
-    private String description;
-    private int mapx;
-    private int mapy;
-    private boolean builtIn;
 
-    public UserData() {
-
-        JSONParser parser = new JSONParser();
-
-        try {
-
-            Object obj = parser.parse(new FileReader("user.json"));
-            JSONObject jObject = (JSONObject) obj;
-
-            POIId = (String) jObject.get("POIId");
-            building = (String) jObject.get("building");
-            buildingCode = (String) jObject.get("buildingCode");
-            floor = (String) jObject.get("floor");
-            roomNumber = (Integer) jObject.get("roomNumber");
-            category = (String) jObject.get("category");
-            description = (String) jObject.get("description");
-            mapx = (Integer) jObject.get("mapX");
-            mapy = (Integer) jObject.get("mapY");
-            builtIn = (Boolean)  jObject.get("builtIn");
-
-
-
-        }
-        catch (FileNotFoundException e) { e.printStackTrace(); }
-        catch (IOException e) { e.printStackTrace(); }
-        catch (ParseException e) { e.printStackTrace(); }
-        catch (Exception e) { e.printStackTrace(); }
-
-    }
 
 
     public void addBookmark(int poiData) {
