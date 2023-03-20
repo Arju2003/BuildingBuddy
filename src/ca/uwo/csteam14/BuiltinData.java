6293a867e7357d1630dae5ab4b6cbac24df3aa1f
Daniel Michael Gomes <dgomes9@uwo.ca>
2023-03-20 17:07:40 -0400

    data stuff

--- src/ca/uwo/csteam14/BuiltinData.java
+++ src/ca/uwo/csteam14/BuiltinData.java
@@ -4,7 +4,6 @@ public class BuiltinData extends Data {
     private String securityKey;
 
     public BuiltinData(String JSONFile) {
-        super(JSONFile);
     }
 
     public void changeKey(String key) {
