603a0013cb31e5e3c53f5adf23cd071ea4c0e2d1
Daniel Michael Gomes <dgomes9@uwo.ca>
2023-03-28 18:39:54 -0400

    POI class javadocs

--- src/main/java/ca/uwo/csteam14/POI.java
+++ src/main/java/ca/uwo/csteam14/POI.java
@@ -1,3 +1,9 @@
+/**
+ * @author Daniel, Bobby
+ * POI class
+ * Construct a POI and instantiate its attributes
+ */
+
 package ca.uwo.csteam14;
 
 public class POI {
@@ -20,48 +26,94 @@ public class POI {
     protected POI next;
 
 
+    /**
+     * @return
+     */
     public int getId() {
         return id;
     }
+
+    /**
+     * @param id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @param building
+     */
     public void setBuilding(String building) {this.building = building;}
 
+    /**
+     * @param map
+     */
     public void setMap(String map) {
         this.map = map;
     }
+
+    /**
+     * @param code
+     */
     public void setCode(String code) { this.code = code;}
+
+    /**
+     * @param floor
+     */
     public void setFloor(String floor) {this.floor = floor;}
 
+    /**
+     * @param category
+     */
     public void setCategory(String category) {
         this.category = category;
     }
 
+    /**
+     * @param roomNumber
+     */
     public void setRoomNumber(String roomNumber) {
         this.roomNumber = roomNumber;
     }
 
+    /**
+     * @param x
+     */
     public void setX(int x) {
         this.positionX = x;
     }
 
+    /**
+     * @param y
+     */
     public void setY(int y) {
         this.positionY = y;
     }
 
+    /**
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @param builtIn
+     */
     public void setBuiltIn(boolean builtIn) {
         this.isBuiltIn = builtIn;
     }
+
+    /**
+     * @param path
+     */
     public void setPath(String path) {
         this.path = path;
     }
@@ -72,6 +124,9 @@ public class POI {
         return this.next;
     }
 
+    /**
+     * @param id
+     */
     public POI(int id) {
         this.id = id;
         name = "";
