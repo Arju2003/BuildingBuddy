d8587214408fa14390f10efed588e5fc9dd91f2d
Daniel Michael Gomes <dgomes9@uwo.ca>
2023-04-04 16:08:57 -0400

    added MapView and Main tests

---
+++ src/main/java/ca/uwo/csteam14/MainTest.java
@@ -0,0 +1,80 @@
+package ca.uwo.csteam14;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This class contains unit tests for the Main class.
+ * @author Daniel Gomes
+ * @version 1.0.0
+ * @since 2023-03-20
+ */
+
+class MainTest {
+
+    /**
+     * This method is called before each test is run to set up any required test data.
+     */
+    @BeforeEach
+    void setUp() {
+    }
+
+    /**
+     * This method is called after each test is run to clean up any test data that was created.
+     */
+    @AfterEach
+    void tearDown() {
+    }
+
+    /**
+     * This method tests the getOptimumPoint() method of the Main class.
+     */
+    @Test
+    void getOptimumPoint() {
+    }
+
+    /**
+     * This method tests the getFloorFullName() method of the Main class.
+     */
+    @Test
+    void getFloorFullName() {
+    }
+
+    /**
+     * This method tests the getBuildingFullName() method of the Main class.
+     */
+    @Test
+    void getBuildingFullName() {
+    }
+
+    /**
+     * This method tests the updateCurrent() method of the Main class.
+     */
+    @Test
+    void updateCurrent() {
+    }
+
+    /**
+     * This method tests the getSecurity() method of the Main class.
+     */
+    @Test
+    void getSecurityKey() {
+    }
+
+    /**
+     * This method tests the changeSecurityKey() method of the Main class.
+     */
+    @Test
+    void changeSecurityKey() {
+    }
+
+    /**
+     * This method tests the updateChecker() method of the Main class.
+     */
+    @Test
+    void updateChecker() {
+    }
+}
