22124692b742e73b13060c7248e2957b79ed4205
Daniel Michael Gomes <dgomes9@uwo.ca>
2023-04-04 16:53:24 -0400

    more test changes

--- src/main/java/ca/uwo/csteam14/MainTest.java
+++ src/main/java/ca/uwo/csteam14/MainTest.java
@@ -4,6 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -34,6 +41,12 @@ class MainTest {
      */
     @Test
     void getOptimumPoint() {
+        System.out.println("getOptimumPoint()");
+
+        Main instance = new Main();
+        Point result = instance.getOptimumPoint("MC");
+        Point expResult = new Point(1700, 700);
+        assertEquals(expResult, result);
     }
 
     /**
@@ -41,6 +54,12 @@ class MainTest {
      */
     @Test
     void getFloorFullName() {
+        System.out.println("getFloorFullName()");
+
+        Main instance = new Main();
+        String result = instance.getFloorFullName("MC0F");
+        String expResult = "Ground Floor";
+        assertEquals(expResult, result);
     }
 
     /**
@@ -48,6 +67,12 @@ class MainTest {
      */
     @Test
     void getBuildingFullName() {
+        System.out.println("getBuildingFloorName()");
+
+        Main instance = new Main();
+        String result = instance.getBuildingFullName("MC0F");
+        String expResult = "Middlesex College";
+        assertEquals(expResult, result);
     }
 
     /**
@@ -55,6 +80,18 @@ class MainTest {
      */
     @Test
     void updateCurrent() {
+        System.out.println("updateCurrent()");
+
+        // Create a POI object with a map attribute
+        POI poi = new POI(1);
+        poi.setMap("MC1F.png");
+
+        // Call the method with the POI object
+        Main.updateCurrent(poi);
+
+        // Check that the current building code and floor have been updated
+        assertEquals("MC", Main.currentBuildingCode);
+        assertEquals("MC1F", Main.currentFloor);
     }
 
     /**
@@ -62,6 +99,14 @@ class MainTest {
      */
     @Test
     void getSecurityKey() {
+        System.out.println("getSecurityKey()");
+
+        Main instance = new Main();
+        char[] result = instance.getSecurityKey();
+        char[] expResult = "CS2212BB".toCharArray();
+
+        assertNotNull(result);
+        assertArrayEquals(expResult, result);
     }
 
     /**
@@ -69,12 +114,23 @@ class MainTest {
      */
     @Test
     void changeSecurityKey() {
-    }
+        System.out.println("changeSecurityKey()");
 
-    /**
-     * This method tests the updateChecker() method of the Main class.
-     */
-    @Test
-    void updateChecker() {
+        Main instance = new Main();
+
+        // careful, this changes the security key
+        char[] newKey = "newkey".toCharArray();
+        instance.changeSecurityKey(newKey);
+
+        // check that the file was updated correctly
+        try (BufferedReader br = new BufferedReader(new FileReader("./data/security_key"))) {
+            char[] savedKey = br.readLine().toCharArray();
+            assertArrayEquals(newKey, savedKey);
+        } catch (IOException e) {
+            fail("IOException: " + e.getMessage());
+        }
+
+        // check that the security key was updated in the instance of the Main class
+        assertArrayEquals(newKey, Main.getSecurityKey());
     }
 }
